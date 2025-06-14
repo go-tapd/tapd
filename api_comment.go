85043e72e4ab6cfe0ffb07f59230288af8457118
Flc <four_leaf_clover@foxmail.com>
2025-06-14 13:50:52 +0800

    refactor(api): simplify comment request structures
    - Reduced the number of fields in GetCommentsRequest and GetCommentsCountRequest
    - Improved code readability by using inline comments
    - Removed unnecessary blank lines and excessive spacing
    
    Signed-off-by: Flc <four_leaf_clover@foxmail.com>

--- api_comment.go
+++ api_comment.go
@@ -51,85 +51,35 @@ type (
 	}
 
 	GetCommentsRequest struct {
-		// 评论ID 支持多ID查询
-		ID *Multi[int64] `url:"id,omitempty"`
-
-		// 标题
-		Title *string `url:"title,omitempty"`
-
-		// 内容
-		Description *string `url:"description,omitempty"`
-
-		// 评论人
-		Author *string `url:"author,omitempty"`
-
-		// 评论类型（取值： bug、 bug_remark （流转缺陷时候的评论）、 stories、 tasks 。多个类型间以竖线隔开） 支持枚举查询
-		EntryType *CommentEntryType `url:"entry_type,omitempty"`
-
-		// 评论所依附的业务对象实体id
-		EntryID *int64 `url:"entry_id,omitempty"`
-
-		// 创建时间 支持时间查询
-		Created *string `url:"created,omitempty"`
-
-		// 最后更改时间 支持时间查询
-		Modified *string `url:"modified,omitempty"`
-
-		// 项目ID
-		WorkspaceID *int `url:"workspace_id,omitempty"`
-
-		// 根评论ID
-		RootID *int64 `url:"root_id,omitempty"`
-
-		// 评论回复的ID
-		ReplyID *int64 `url:"reply_id,omitempty"`
-
-		// 设置返回数量限制，默认为30
-		Limit *int `url:"limit,omitempty"`
-
-		// 返回当前数量限制下第N页的数据，默认为1（第一页）
-		Page *int `url:"page,omitempty"`
-
-		// 排序规则，规则：字段名 ASC或者DESC，然后 urlencode 如按创建时间逆序：order=created%20desc
-		Order *Order `url:"order,omitempty"`
-
-		// 设置获取的字段，多个字段间以','逗号隔开
-		Fields *Multi[string] `url:"fields,omitempty"`
+		ID          *Multi[int64]     `url:"id,omitempty"`           // 评论ID 支持多ID查询
+		Title       *string           `url:"title,omitempty"`        // 标题
+		Description *string           `url:"description,omitempty"`  // 内容
+		Author      *string           `url:"author,omitempty"`       // 评论人
+		EntryType   *CommentEntryType `url:"entry_type,omitempty"`   // 评论类型（取值： bug、 bug_remark （流转缺陷时候的评论）、 stories、 tasks 。多个类型间以竖线隔开） 支持枚举查询
+		EntryID     *int64            `url:"entry_id,omitempty"`     // 评论所依附的业务对象实体id
+		Created     *string           `url:"created,omitempty"`      // 创建时间 支持时间查询
+		Modified    *string           `url:"modified,omitempty"`     // 最后更改时间 支持时间查询
+		WorkspaceID *int              `url:"workspace_id,omitempty"` // 项目ID
+		RootID      *int64            `url:"root_id,omitempty"`      // 根评论ID
+		ReplyID     *int64            `url:"reply_id,omitempty"`     // 评论回复的ID
+		Limit       *int              `url:"limit,omitempty"`        // 设置返回数量限制，默认为30
+		Page        *int              `url:"page,omitempty"`         // 返回当前数量限制下第N页的数据，默认为1（第一页）
+		Order       *Order            `url:"order,omitempty"`        // 排序规则，规则：字段名 ASC或者DESC，然后 urlencode 如按创建时间逆序：order=created%20desc
+		Fields      *Multi[string]    `url:"fields,omitempty"`       // 设置获取的字段，多个字段间以','逗号隔开
 	}
 
 	GetCommentsCountRequest struct {
-		// 评论ID 支持多ID查询
-		ID *Multi[int64] `url:"id,omitempty"`
-
-		// 标题
-		Title *string `url:"title,omitempty"`
-
-		// 内容
-		Description *string `url:"description,omitempty"`
-
-		// 评论人
-		Author *string `url:"author,omitempty"`
-
-		// 评论类型（取值： bug、 bug_remark （流转缺陷时候的评论）、 stories、 tasks 。多个类型间以竖线隔开） 支持枚举查询
-		EntryType *CommentEntryType `url:"entry_type,omitempty"`
-
-		// 评论所依附的业务对象实体id
-		EntryID *int64 `url:"entry_id,omitempty"`
-
-		// 创建时间 支持时间查询
-		Created *string `url:"created,omitempty"`
-
-		// 最后更改时间 支持时间查询
-		Modified *string `url:"modified,omitempty"`
-
-		// 项目ID
-		WorkspaceID *int `url:"workspace_id,omitempty"`
-
-		// 根评论ID
-		RootID *int64 `url:"root_id,omitempty"`
-
-		// 评论回复的ID
-		ReplyID *int64 `url:"reply_id,omitempty"`
+		ID          *Multi[int64]     `url:"id,omitempty"`           // 评论ID 支持多ID查询
+		Title       *string           `url:"title,omitempty"`        // 标题
+		Description *string           `url:"description,omitempty"`  // 内容
+		Author      *string           `url:"author,omitempty"`       // 评论人
+		EntryType   *CommentEntryType `url:"entry_type,omitempty"`   // 评论类型（取值： bug、 bug_remark （流转缺陷时候的评论）、 stories、 tasks 。多个类型间以竖线隔开） 支持枚举查询
+		EntryID     *int64            `url:"entry_id,omitempty"`     // 评论所依附的业务对象实体id
+		Created     *string           `url:"created,omitempty"`      // 创建时间 支持时间查询
+		Modified    *string           `url:"modified,omitempty"`     // 最后更改时间 支持时间查询
+		WorkspaceID *int              `url:"workspace_id,omitempty"` // 项目ID
+		RootID      *int64            `url:"root_id,omitempty"`      // 根评论ID
+		ReplyID     *int64            `url:"reply_id,omitempty"`     // 评论回复的ID
 	}
 
 	UpdateCommentRequest struct {
