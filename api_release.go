d878d661782028b5d4b276e09ecda3e3253eecff
FLC <i@flc.io>
2026-05-16 23:46:45 +0800

    feat(release): add support for launch forms and accessories APIs
    - Define LaunchForm, LaunchAccessory, and related request structs for release review feature
    - Implement GetLaunchForms, CreateLaunchForm, GetLaunchAccessories, and CreateLaunchAccessory methods
    - Add API endpoints integration for managing release launch forms and their accessory evidences
    - Include comprehensive unit tests validating new launch forms and accessories functionalities
    - Update feature documentation to mark launch form and accessory support as implemented but unverified manually

--- api_release.go
+++ api_release.go
@@ -67,6 +67,108 @@ type (
 		EndDate     *string `json:"enddate,omitempty"`      // 结束时间
 		Status      *string `json:"status,omitempty"`       // 状态
 	}
+
+	LaunchForm struct {
+		ID             string  `json:"id,omitempty"`              // 评审ID
+		WorkspaceID    string  `json:"workspace_id,omitempty"`    // 所属项目ID
+		Created        string  `json:"created,omitempty"`         // 创建时间
+		Title          *string `json:"title,omitempty"`           // 标题
+		Name           string  `json:"name,omitempty"`            // 名称
+		Creator        string  `json:"creator,omitempty"`         // 创建人
+		Status         string  `json:"status,omitempty"`          // 状态
+		VersionType    *string `json:"version_type,omitempty"`    // 版本类型
+		Baseline       *string `json:"baseline,omitempty"`        // 基线
+		ReleaseModel   *string `json:"release_model,omitempty"`   // 发布模块
+		RoadmapVersion *string `json:"roadmap_version,omitempty"` // 路标版本
+		ReleaseType    *string `json:"release_type,omitempty"`    // 发布类型
+		ChangeType     *string `json:"change_type,omitempty"`     // 变更类型
+		SignedBy       *string `json:"signed_by,omitempty"`       // 签发人
+		ArchivedBy     *string `json:"archived_by,omitempty"`     // 发布确认人
+		CC             *string `json:"cc,omitempty"`              // 抄送人
+		ChangeNotifier *string `json:"change_notifier,omitempty"` // 变更通知人
+		Signed         *string `json:"signed,omitempty"`          // 签发时间
+		Archived       *string `json:"archived,omitempty"`        // 归档时间
+		SignerResult   *string `json:"signer_result,omitempty"`   // 签发结论
+		SignerComment  *string `json:"signer_comment,omitempty"`  // 签发意见
+		ReleaseResult  *string `json:"release_result,omitempty"`  // 发布结果
+		ReleaseComment *string `json:"release_comment,omitempty"` // 发布意见
+		TestPath       *string `json:"test_path,omitempty"`       // 测试路径
+		CreatedPath    *string `json:"created_path,omitempty"`    // 归档路径
+		Remark         *string `json:"remark,omitempty"`          // 备注
+		Participator   *string `json:"participator,omitempty"`    // 参与人
+		TemplateID     string  `json:"template_id,omitempty"`     // 模板ID
+		IterationID    *string `json:"iteration_id,omitempty"`    // 迭代ID
+		ReleaseID      *string `json:"release_id,omitempty"`      // 发布计划ID
+		Flows          string  `json:"flows,omitempty"`           // 流程状态
+	}
+
+	GetLaunchFormsRequest struct {
+		WorkspaceID    *int           `url:"workspace_id,omitempty"`    // [必须]项目ID
+		ID             *int64         `url:"id,omitempty"`              // 发布评审ID
+		Creator        *string        `url:"creator,omitempty"`         // 创建人
+		Created        *string        `url:"created,omitempty"`         // 创建时间，支持时间查询
+		Title          *string        `url:"title,omitempty"`           // 标题
+		Status         *string        `url:"status,omitempty"`          // 状态
+		VersionType    *string        `url:"version_type,omitempty"`    // 版本类型
+		Baseline       *string        `url:"baseline,omitempty"`        // 基线
+		ReleaseModel   *string        `url:"release_model,omitempty"`   // 发布模块
+		RoadmapVersion *string        `url:"roadmap_version,omitempty"` // 路标版本
+		ReleaseType    *string        `url:"release_type,omitempty"`    // 发布类型
+		ChangeType     *string        `url:"change_type,omitempty"`     // 变更类型
+		SignedBy       *string        `url:"signed_by,omitempty"`       // 签发人
+		ArchivedBy     *string        `url:"archived_by,omitempty"`     // 发布确认人
+		CC             *string        `url:"cc,omitempty"`              // 抄送人
+		ChangeNotifier *string        `url:"change_notifier,omitempty"` // 变更通知人
+		Limit          *int           `url:"limit,omitempty"`           // 设置返回数量限制，默认为30，最大取200
+		Page           *int           `url:"page,omitempty"`            // 返回当前数量限制下第N页的数据，默认为1
+		Fields         *Multi[string] `url:"fields,omitempty"`          // 设置获取的字段，多个字段间以','逗号隔开
+	}
+
+	CreateLaunchFormRequest struct {
+		WorkspaceID    *int    `json:"workspace_id,omitempty"`    // [必须]项目ID
+		Creator        *string `json:"creator,omitempty"`         // [必须]创建人
+		TemplateID     *string `json:"template_id,omitempty"`     // [必须]模板ID
+		Title          *string `json:"title,omitempty"`           // 标题
+		VersionType    *string `json:"version_type,omitempty"`    // 版本类型
+		Baseline       *string `json:"baseline,omitempty"`        // 基线
+		ReleaseModel   *string `json:"release_model,omitempty"`   // 发布模块
+		RoadmapVersion *string `json:"roadmap_version,omitempty"` // 路标版本
+		ReleaseType    *string `json:"release_type,omitempty"`    // 发布类型
+		SignedBy       *string `json:"signed_by,omitempty"`       // 签发人
+		ArchivedBy     *string `json:"archived_by,omitempty"`     // 发布确认人
+		CC             *string `json:"cc,omitempty"`              // 抄送人
+	}
+
+	LaunchAccessory struct {
+		ID          string  `json:"id,omitempty"`           // 依据ID
+		FormID      string  `json:"form_id,omitempty"`      // 发布评审ID
+		WorkspaceID string  `json:"workspace_id,omitempty"` // 所属项目ID
+		Type        string  `json:"type,omitempty"`         // 类型
+		Tag         *string `json:"tag,omitempty"`          // 标签
+		Title       string  `json:"title,omitempty"`        // 标题
+		Content     string  `json:"content,omitempty"`      // 内容
+		Description *string `json:"description,omitempty"`  // 详细描述
+		ContentType *string `json:"content_type,omitempty"` // 内容类型
+		CreatedBy   string  `json:"created_by,omitempty"`   // 创建人
+		Created     string  `json:"created,omitempty"`      // 创建时间
+		GroupID     *string `json:"group_id,omitempty"`     // 分组ID
+		Source      string  `json:"source,omitempty"`       // 来源
+	}
+
+	GetLaunchAccessoriesRequest struct {
+		WorkspaceID *int    `url:"workspace_id,omitempty"` // [必须]项目ID
+		FormID      *int64  `url:"form_id,omitempty"`      // [必须]评审单ID
+		ID          *int64  `url:"id,omitempty"`           // 评审依据ID
+		CreatedBy   *string `url:"created_by,omitempty"`   // 创建人
+		Created     *string `url:"created,omitempty"`      // 创建时间，支持时间查询
+	}
+
+	CreateLaunchAccessoryRequest struct {
+		WorkspaceID *int    `json:"workspace_id,omitempty"` // [必须]项目ID
+		FormID      *int64  `json:"form_id,omitempty"`      // [必须]发布评审ID
+		Type        *string `json:"type,omitempty"`         // [必须]类型，仅支持 launch_url
+		Content     *string `json:"content,omitempty"`      // [必须]url 地址
+	}
 )
 
 // ReleaseService 发布服务。
@@ -92,6 +194,26 @@ type ReleaseService interface {
 	//
 	// https://open.tapd.cn/document/api-doc/API%E6%96%87%E6%A1%A3/api_reference/release/update_new_release.html
 	UpdateRelease(ctx context.Context, request *UpdateReleaseRequest, opts ...RequestOption) (*Release, *Response, error)
+
+	// GetLaunchAccessories 获取发布评审依据
+	//
+	// https://open.tapd.cn/document/api-doc/API%E6%96%87%E6%A1%A3/api_reference/release/get_launch_accessories.html
+	GetLaunchAccessories(ctx context.Context, request *GetLaunchAccessoriesRequest, opts ...RequestOption) ([]*LaunchAccessory, *Response, error)
+
+	// GetLaunchForms 获取发布评审
+	//
+	// https://open.tapd.cn/document/api-doc/API%E6%96%87%E6%A1%A3/api_reference/release/get_launch_forms.html
+	GetLaunchForms(ctx context.Context, request *GetLaunchFormsRequest, opts ...RequestOption) ([]*LaunchForm, *Response, error)
+
+	// CreateLaunchForm 创建发布评审
+	//
+	// https://open.tapd.cn/document/api-doc/API%E6%96%87%E6%A1%A3/api_reference/release/add_launch_form.html
+	CreateLaunchForm(ctx context.Context, request *CreateLaunchFormRequest, opts ...RequestOption) (*LaunchForm, *Response, error)
+
+	// CreateLaunchAccessory 创建发布评审依据
+	//
+	// https://open.tapd.cn/document/api-doc/API%E6%96%87%E6%A1%A3/api_reference/release/add_launch_accessories.html
+	CreateLaunchAccessory(ctx context.Context, request *CreateLaunchAccessoryRequest, opts ...RequestOption) (*LaunchAccessory, *Response, error)
 }
 
 type releaseService struct {
@@ -183,4 +305,90 @@ func (s *releaseService) UpdateRelease(
 	}
 
 	return response.Release, resp, nil
+}
+
+func (s *releaseService) GetLaunchAccessories(
+	ctx context.Context, request *GetLaunchAccessoriesRequest, opts ...RequestOption,
+) ([]*LaunchAccessory, *Response, error) {
+	req, err := s.client.NewRequest(ctx, http.MethodGet, "launch_accessories", request, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var items []struct {
+		LaunchAccessory *LaunchAccessory `json:"LaunchAccessory"`
+	}
+	resp, err := s.client.Do(req, &items)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	accessories := make([]*LaunchAccessory, 0, len(items))
+	for _, item := range items {
+		accessories = append(accessories, item.LaunchAccessory)
+	}
+
+	return accessories, resp, nil
+}
+
+func (s *releaseService) GetLaunchForms(
+	ctx context.Context, request *GetLaunchFormsRequest, opts ...RequestOption,
+) ([]*LaunchForm, *Response, error) {
+	req, err := s.client.NewRequest(ctx, http.MethodGet, "launch_forms", request, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var items []struct {
+		LaunchForm *LaunchForm `json:"LaunchForm"`
+	}
+	resp, err := s.client.Do(req, &items)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	forms := make([]*LaunchForm, 0, len(items))
+	for _, item := range items {
+		forms = append(forms, item.LaunchForm)
+	}
+
+	return forms, resp, nil
+}
+
+func (s *releaseService) CreateLaunchForm(
+	ctx context.Context, request *CreateLaunchFormRequest, opts ...RequestOption,
+) (*LaunchForm, *Response, error) {
+	req, err := s.client.NewRequest(ctx, http.MethodPost, "launch_forms", request, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var response struct {
+		LaunchForm *LaunchForm `json:"LaunchForm"`
+	}
+	resp, err := s.client.Do(req, &response)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return response.LaunchForm, resp, nil
+}
+
+func (s *releaseService) CreateLaunchAccessory(
+	ctx context.Context, request *CreateLaunchAccessoryRequest, opts ...RequestOption,
+) (*LaunchAccessory, *Response, error) {
+	req, err := s.client.NewRequest(ctx, http.MethodPost, "launch_accessories", request, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var response struct {
+		LaunchAccessory *LaunchAccessory `json:"LaunchAccessory"`
+	}
+	resp, err := s.client.Do(req, &response)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return response.LaunchAccessory, resp, nil
 }
--- api_release_test.go
+++ api_release_test.go
@@ -167,3 +167,179 @@ func TestReleaseService_UpdateRelease(t *testing.T) {
 	assert.Equal(t, "2020-10-27 11:24:48", release.Modified)
 	assert.Equal(t, "open", release.Status)
 }
+
+func TestReleaseService_GetLaunchAccessories(t *testing.T) {
+	_, client := createServerClient(t, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		assert.Equal(t, "/launch_accessories", r.URL.Path)
+		assert.Equal(t, "10104801", r.URL.Query().Get("workspace_id"))
+		assert.Equal(t, "1010104801000402051", r.URL.Query().Get("form_id"))
+		assert.Equal(t, "1010104801000253485", r.URL.Query().Get("id"))
+		assert.Equal(t, "v_xuanfang", r.URL.Query().Get("created_by"))
+		assert.Equal(t, "2020-06-11", r.URL.Query().Get("created"))
+
+		_, _ = w.Write(loadData(t, "internal/testdata/api/release/get_launch_accessories.json"))
+	}))
+
+	accessories, _, err := client.ReleaseService.GetLaunchAccessories(ctx, &GetLaunchAccessoriesRequest{
+		WorkspaceID: Ptr(10104801),
+		FormID:      Ptr[int64](1010104801000402051),
+		ID:          Ptr[int64](1010104801000253485),
+		CreatedBy:   Ptr("v_xuanfang"),
+		Created:     Ptr("2020-06-11"),
+	})
+	assert.NoError(t, err)
+	assert.Len(t, accessories, 2)
+	assert.Equal(t, "1010104801000253485", accessories[0].ID)
+	assert.Equal(t, "1010104801000402051", accessories[0].FormID)
+	assert.Equal(t, "10104801", accessories[0].WorkspaceID)
+	assert.Equal(t, "launch_tasks_list", accessories[0].Type)
+	assert.Equal(t, "任务列表", accessories[0].Title)
+	assert.Equal(t, "1010104801500601739", accessories[0].Content)
+	assert.Equal(t, "task", *accessories[0].ContentType)
+	assert.Equal(t, "v_xuanfang", accessories[0].CreatedBy)
+	assert.Equal(t, "2020-06-11 16:17:56", accessories[0].Created)
+}
+
+func TestReleaseService_GetLaunchForms(t *testing.T) {
+	_, client := createServerClient(t, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		assert.Equal(t, "/launch_forms", r.URL.Path)
+		assert.Equal(t, "10104801", r.URL.Query().Get("workspace_id"))
+		assert.Equal(t, "1010104801079697767", r.URL.Query().Get("id"))
+		assert.Equal(t, "v_xuanfang", r.URL.Query().Get("creator"))
+		assert.Equal(t, "2021-01-15", r.URL.Query().Get("created"))
+		assert.Equal(t, "发布评审", r.URL.Query().Get("title"))
+		assert.Equal(t, "LAUNCHFORM_STATUS_INITIAL", r.URL.Query().Get("status"))
+		assert.Equal(t, "version", r.URL.Query().Get("version_type"))
+		assert.Equal(t, "baseline", r.URL.Query().Get("baseline"))
+		assert.Equal(t, "module", r.URL.Query().Get("release_model"))
+		assert.Equal(t, "roadmap", r.URL.Query().Get("roadmap_version"))
+		assert.Equal(t, "正常发布", r.URL.Query().Get("release_type"))
+		assert.Equal(t, "normal", r.URL.Query().Get("change_type"))
+		assert.Equal(t, "signer", r.URL.Query().Get("signed_by"))
+		assert.Equal(t, "archiver", r.URL.Query().Get("archived_by"))
+		assert.Equal(t, "cc", r.URL.Query().Get("cc"))
+		assert.Equal(t, "notifier", r.URL.Query().Get("change_notifier"))
+		assert.Equal(t, "10", r.URL.Query().Get("limit"))
+		assert.Equal(t, "1", r.URL.Query().Get("page"))
+		assert.Equal(t, "id,title,workspace_id", r.URL.Query().Get("fields"))
+
+		_, _ = w.Write(loadData(t, "internal/testdata/api/release/get_launch_forms.json"))
+	}))
+
+	forms, _, err := client.ReleaseService.GetLaunchForms(ctx, &GetLaunchFormsRequest{
+		WorkspaceID:    Ptr(10104801),
+		ID:             Ptr[int64](1010104801079697767),
+		Creator:        Ptr("v_xuanfang"),
+		Created:        Ptr("2021-01-15"),
+		Title:          Ptr("发布评审"),
+		Status:         Ptr("LAUNCHFORM_STATUS_INITIAL"),
+		VersionType:    Ptr("version"),
+		Baseline:       Ptr("baseline"),
+		ReleaseModel:   Ptr("module"),
+		RoadmapVersion: Ptr("roadmap"),
+		ReleaseType:    Ptr("正常发布"),
+		ChangeType:     Ptr("normal"),
+		SignedBy:       Ptr("signer"),
+		ArchivedBy:     Ptr("archiver"),
+		CC:             Ptr("cc"),
+		ChangeNotifier: Ptr("notifier"),
+		Limit:          Ptr(10),
+		Page:           Ptr(1),
+		Fields:         NewMulti("id", "title", "workspace_id"),
+	})
+	assert.NoError(t, err)
+	assert.Len(t, forms, 1)
+	assert.Equal(t, "1010104801079697767", forms[0].ID)
+	assert.Nil(t, forms[0].Title)
+	assert.Equal(t, "202101150008", forms[0].Name)
+	assert.Equal(t, "v_xuanfang", forms[0].Creator)
+	assert.Equal(t, "10104801", forms[0].WorkspaceID)
+	assert.Equal(t, "LAUNCHFORM_STATUS_INITIAL", forms[0].Status)
+	assert.Equal(t, "正常发布", *forms[0].ReleaseType)
+	assert.Equal(t, ";v_xuanfang;", *forms[0].Participator)
+	assert.Equal(t, "1010104801065798331", forms[0].TemplateID)
+	assert.Equal(t, "LAUNCHFORM_STATUS_INITIAL", forms[0].Flows)
+}
+
+func TestReleaseService_CreateLaunchForm(t *testing.T) {
+	_, client := createServerClient(t, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+		assert.Equal(t, "/launch_forms", r.URL.Path)
+
+		var req CreateLaunchFormRequest
+		assert.NoError(t, json.NewDecoder(r.Body).Decode(&req))
+		assert.Equal(t, 10104801, *req.WorkspaceID)
+		assert.Equal(t, "tapd_api", *req.Creator)
+		assert.Equal(t, "1010104801065798331", *req.TemplateID)
+		assert.Equal(t, "发布评审", *req.Title)
+		assert.Equal(t, "version", *req.VersionType)
+		assert.Equal(t, "baseline", *req.Baseline)
+		assert.Equal(t, "module", *req.ReleaseModel)
+		assert.Equal(t, "roadmap", *req.RoadmapVersion)
+		assert.Equal(t, "正常发布", *req.ReleaseType)
+		assert.Equal(t, "signer", *req.SignedBy)
+		assert.Equal(t, "archiver", *req.ArchivedBy)
+		assert.Equal(t, "cc", *req.CC)
+
+		_, _ = w.Write(loadData(t, "internal/testdata/api/release/create_launch_form.json"))
+	}))
+
+	form, _, err := client.ReleaseService.CreateLaunchForm(ctx, &CreateLaunchFormRequest{
+		WorkspaceID:    Ptr(10104801),
+		Creator:        Ptr("tapd_api"),
+		TemplateID:     Ptr("1010104801065798331"),
+		Title:          Ptr("发布评审"),
+		VersionType:    Ptr("version"),
+		Baseline:       Ptr("baseline"),
+		ReleaseModel:   Ptr("module"),
+		RoadmapVersion: Ptr("roadmap"),
+		ReleaseType:    Ptr("正常发布"),
+		SignedBy:       Ptr("signer"),
+		ArchivedBy:     Ptr("archiver"),
+		CC:             Ptr("cc"),
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, "1010104801079724013", form.ID)
+	assert.Nil(t, form.Title)
+	assert.Equal(t, "202107210009", form.Name)
+	assert.Equal(t, "v_xuanfang", form.Creator)
+	assert.Equal(t, "10104801", form.WorkspaceID)
+	assert.Equal(t, "initial", form.Status)
+	assert.Equal(t, "正常发布", *form.ReleaseType)
+	assert.Equal(t, ";v_xuanfang;", *form.Participator)
+	assert.Equal(t, "1010104801065798331", form.TemplateID)
+}
+
+func TestReleaseService_CreateLaunchAccessory(t *testing.T) {
+	_, client := createServerClient(t, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+		assert.Equal(t, "/launch_accessories", r.URL.Path)
+
+		var req CreateLaunchAccessoryRequest
+		assert.NoError(t, json.NewDecoder(r.Body).Decode(&req))
+		assert.Equal(t, 10104801, *req.WorkspaceID)
+		assert.Equal(t, int64(1010104801079533889), *req.FormID)
+		assert.Equal(t, "launch_url", *req.Type)
+		assert.Equal(t, "https://www.tapd.cn/", *req.Content)
+
+		_, _ = w.Write(loadData(t, "internal/testdata/api/release/create_launch_accessory.json"))
+	}))
+
+	accessory, _, err := client.ReleaseService.CreateLaunchAccessory(ctx, &CreateLaunchAccessoryRequest{
+		WorkspaceID: Ptr(10104801),
+		FormID:      Ptr[int64](1010104801079533889),
+		Type:        Ptr("launch_url"),
+		Content:     Ptr("https://www.tapd.cn/"),
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, "1010104801000254035", accessory.ID)
+	assert.Equal(t, "1010104801079533889", accessory.FormID)
+	assert.Equal(t, "10104801", accessory.WorkspaceID)
+	assert.Equal(t, "launch_url", accessory.Type)
+	assert.Equal(t, "URL", accessory.Title)
+	assert.Equal(t, "https://www.tapd.cn/", accessory.Content)
+	assert.Equal(t, "tapd", accessory.CreatedBy)
+	assert.Equal(t, "2022-09-08 16:45:30", accessory.Created)
+}
